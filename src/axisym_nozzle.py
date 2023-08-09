51a2f784ad111d20de34b55089809187b367326c
AMilkyBoi <30729007+AMilkyBoi@users.noreply.github.com>
2023-08-08 20:31:23 -0500

    testing push to dev branch

---
+++ src/axisym_nozzle.py
@@ -0,0 +1,12 @@
+# module axisym_nozzle
+'''
+Axisymmetric supersonic nozzle design using the 2-D method of characteristics.
+'''
+
+def main():
+    '''
+    Runs the program.
+    '''
+
+if __name__ == '__main__':
+    main()
